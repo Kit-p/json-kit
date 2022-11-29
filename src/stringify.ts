857a787a7a69d0466e3cadf53b3b2f9d3a57611d
Kit-p <pangkit888@gmail.com>
2022-11-29 19:48:21 +0000

    test(feat-WIP): implement minification
    Signed-off-by: Kit-p <pangkit888@gmail.com>

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,17 +1,24 @@
-import { EJSON } from 'bson';
+import { deserialize, EJSON, serialize } from 'bson';
+import {
+  MINIFY_REMAINING_CANDIDATES,
+  MINIFY_STARTING_CANDIDATES,
+} from './constants';
 
 export type StringifyReplacer = (this: any, key: string, value: any) => any;
 
 export interface StringifyOptions {
   extended?: boolean | { enable: boolean; relaxed?: boolean };
+  minify?: false | { whitespace?: boolean; key?: boolean };
 }
 
 interface _StringifyOptions {
   extended: { enable: boolean; relaxed: boolean };
+  minify: { whitespace: boolean; key: boolean };
 }
 
 const defaultOptions: _StringifyOptions = {
   extended: { enable: false, relaxed: true },
+  minify: { whitespace: false, key: false },
 };
 
 function mergeWithDefaultOptions(input?: StringifyOptions): _StringifyOptions {
@@ -31,6 +38,20 @@ function mergeWithDefaultOptions(input?: StringifyOptions): _StringifyOptions {
     };
   }
 
+  if (input.minify == null) {
+    input.minify = defaultOptions.minify;
+  } else if (typeof input.minify === 'boolean') {
+    input.minify = {
+      whitespace: input.minify,
+      key: defaultOptions.minify.key,
+    };
+  } else {
+    input.minify = {
+      whitespace: input.minify.whitespace ?? defaultOptions.minify.whitespace,
+      key: input.minify.key ?? defaultOptions.minify.key,
+    };
+  }
+
   return input as _StringifyOptions;
 }
 
@@ -43,12 +64,20 @@ export function stringify(
   let _replacer: StringifyReplacer | undefined = undefined;
   if (replacer != null && typeof replacer === 'function') {
     _replacer = replacer satisfies StringifyReplacer;
-  } else {
+  } else if (options == null) {
     options = (replacer ?? undefined) satisfies StringifyOptions | undefined;
     replacer = undefined;
   }
 
   const _options = mergeWithDefaultOptions(options);
+
+  if (_options.minify.whitespace) {
+    space = undefined;
+  }
+
+  if (_options.minify.key) {
+    obj = minifyJsonKeys(obj);
+  }
 
   if (_options.extended.enable) {
     return EJSON.stringify(obj, _replacer, space, {
@@ -56,4 +85,78 @@ export function stringify(
     });
   }
   return JSON.stringify(obj, _replacer, space);
+}
+
+export interface KeyMinifiedJson<T> {
+  _jkv: T;
+  _jkm: Record<string, string>;
+}
+
+function minifyJsonKeys<T>(obj: T): KeyMinifiedJson<T> {
+  obj = deserialize(serialize(obj as any, { ignoreUndefined: true })) as T;
+
+  const keyMap: Record<string, string> = {};
+  const reverseKeyMap: Record<string, string> = {};
+  const objMaps: Array<{ obj: any; key: string; value: any }> = [];
+  const allKeys: string[] = [];
+  const queue: any[] = [obj];
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    if (current == null) {
+      continue;
+    }
+
+    if (Array.isArray(current)) {
+      queue.push(...current);
+      continue;
+    }
+
+    if (current.constructor === Object) {
+      for (const [key, value] of Object.entries(current)) {
+        objMaps.push({ obj: current, key, value });
+        allKeys.push(key);
+        queue.push(value);
+      }
+    }
+
+    // ignore primitives, functions, and custom objects (e.g. Date)
+  }
+
+  const startChoices = MINIFY_STARTING_CANDIDATES.length;
+  const remainChoices = MINIFY_REMAINING_CANDIDATES.length;
+  let idx = 0;
+  for (let i = 0; i < allKeys.length; i++) {
+    const key = allKeys[i];
+
+    if (reverseKeyMap[key] != null) {
+      continue;
+    }
+
+    let minifiedKey: string;
+    if (idx < startChoices - 1) {
+      minifiedKey = MINIFY_STARTING_CANDIDATES[idx];
+    } else {
+      const _idx = idx - startChoices;
+      const startIdx = Math.floor(_idx / remainChoices);
+      const remainIdx = _idx % remainChoices;
+      minifiedKey = `${MINIFY_STARTING_CANDIDATES[startIdx]}${MINIFY_REMAINING_CANDIDATES[remainIdx]}`;
+    }
+
+    keyMap[minifiedKey] = key;
+    reverseKeyMap[key] = minifiedKey;
+    ++idx;
+  }
+
+  for (const { obj, key, value } of objMaps) {
+    // TODO: fix overwriting data and deleting wrong data if key name crash
+    const minifiedKey = reverseKeyMap[key];
+    obj[minifiedKey] = value;
+    delete obj[key];
+  }
+
+  return {
+    _jkv: obj,
+    _jkm: keyMap,
+  };
 }
--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -2,7 +2,7 @@ import { EJSON } from 'bson';
 import JsonKit from '../src';
 import type { StringifyReplacer } from '../src/stringify';
 
-describe('stringify', () => {
+describe('[stringify] basic', () => {
   const obj = {
     a: 1,
     '2': '2',
@@ -66,5 +66,97 @@ describe('stringify', () => {
     expect(optionsExtended).toEqual(optionsExtendedFull);
 
     expect(optionsExtendedDisable).toEqual(defaultOptions);
+  });
+});
+
+describe('[stringify] minify', () => {
+  const obj = {
+    'very-long-name': {
+      'very-long-name': 'very-long-name',
+      'very-long-name-2': 2,
+    },
+    number: 1,
+    boolean: true,
+    null: null,
+    undefined: undefined,
+    function: () => 'string',
+    date: new Date(),
+    arr: [
+      { 'very-long-name': { 'very-long-name': 'very-long-name' } },
+      {
+        'very-long-name-2': true,
+        testKeys: {
+          a: 1,
+          b: 2,
+          c: 3,
+          d: 4,
+          e: 5,
+          f: 6,
+          g: 7,
+          h: 8,
+          i: 9,
+          j: 10,
+          k: 11,
+          l: 12,
+          m: 13,
+          n: 14,
+          o: 15,
+          p: 16,
+          q: 17,
+          r: 18,
+          s: 19,
+          t: 20,
+          u: 21,
+          v: 22,
+          w: 23,
+          x: 24,
+          y: 25,
+          z: 26,
+          1: 'a',
+          2: 'b',
+          3: 'c',
+          4: 'd',
+          5: 'e',
+          6: 'f',
+          7: 'g',
+          8: 'h',
+          9: 'i',
+          10: 'j',
+          11: 'k',
+          12: 'l',
+          13: 'm',
+          14: 'n',
+          15: 'o',
+          16: 'p',
+          17: 'q',
+          18: 'r',
+          19: 's',
+          20: 't',
+          21: 'u',
+          22: 'v',
+          23: 'w',
+          24: 'x',
+          25: 'y',
+          26: 'z',
+        },
+      },
+    ],
+    normal: 'string',
+  };
+
+  it('stringify minify with whitespace only', () => {
+    expect(
+      JsonKit.stringify(obj, null, 2, { minify: { whitespace: true } })
+    ).toEqual(JSON.stringify(obj));
+
+    expect(
+      JsonKit.stringify(obj, null, 2, { minify: { whitespace: false } })
+    ).toEqual(JSON.stringify(obj, null, 2));
+  });
+
+  it('stringify minify with whitespace and key', () => {
+    expect(
+      JsonKit.stringify(obj, { minify: { whitespace: true, key: true } })
+    ).toEqual('');
   });
 });
