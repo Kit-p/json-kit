2a1be1369ca15a37212d71ee14144be30f6962a3
Kit-p <pangkit888@gmail.com>
2022-12-01 13:51:29 +0000

    test: increase converage to 100%
    Signed-off-by: Kit-p <pangkit888@gmail.com>

--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -48,24 +48,30 @@ describe('[stringify] basic', () => {
   it('stringify default options should match docs', () => {
     const base = JsonKit.stringify(obj);
     const emptyOptions = JsonKit.stringify(obj, {});
-    const optionsExtended = JsonKit.stringify(obj, { extended: true });
-    const optionsExtendedDisable = JsonKit.stringify(obj, {
-      extended: { enable: false },
+    const optionsShorthand = JsonKit.stringify(obj, {
+      extended: true,
+      minify: false,
     });
-    const optionsExtendedFull = JsonKit.stringify(obj, {
+    const optionsFull = JsonKit.stringify(obj, {
+      extended: { enable: false },
+      minify: { whitespace: false, key: false },
+    });
+    const optionsDefaultFull = JsonKit.stringify(obj, {
       extended: { enable: true, relaxed: true },
+      minify: { whitespace: false, key: false },
     });
     const defaultOptions = JsonKit.stringify(obj, {
       extended: { enable: false, relaxed: true },
+      minify: { whitespace: false, key: false },
     });
 
     expect(base).toEqual(defaultOptions);
 
     expect(emptyOptions).toEqual(defaultOptions);
 
-    expect(optionsExtended).toEqual(optionsExtendedFull);
+    expect(optionsShorthand).toEqual(optionsDefaultFull);
 
-    expect(optionsExtendedDisable).toEqual(defaultOptions);
+    expect(optionsFull).toEqual(defaultOptions);
   });
 });
 
@@ -154,10 +160,8 @@ describe('[stringify] minify', () => {
     ).toEqual(JSON.stringify(obj, null, 2));
   });
 
-  it('stringify minify with whitespace and key', () => {
-    expect(
-      JsonKit.stringify(obj, { minify: { whitespace: true, key: true } })
-    ).toEqual(
+  it('stringify minify with key', () => {
+    expect(JsonKit.stringify(obj, { minify: { key: true } })).toEqual(
       '{"_jkv":{"a0":{"a0":"very-long-name","a7":2},"a1":1,"a2":true,"a3":null,"a4":"2022-12-01T13:44:15.177Z","a5":[{"a0":{"a0":"very-long-name"}},{"a7":true,"a8":{"a9":"a","aa":"b","ab":"c","ac":"d","ad":"e","ae":"f","af":"g","ag":"h","ah":"i","ai":"j","aj":"k","ak":"l","al":"m","am":"n","an":"o","ao":"p","ap":"q","aq":"r","ar":"s","as":"t","at":"u","au":"v","av":"w","aw":"x","ax":"y","ay":"z","az":1,"b0":2,"b1":3,"b2":4,"b3":5,"b4":6,"b5":7,"b6":8,"b7":9,"b8":10,"b9":11,"ba":12,"bb":13,"bc":14,"bd":15,"be":16,"bf":17,"bg":18,"bh":19,"bi":20,"bj":21,"bk":22,"bl":23,"bm":24,"bn":25,"bo":26}}],"a6":"string"},"_jkm":{"a0":"very-long-name","a1":"number","a2":"boolean","a3":"null","a4":"date","a5":"arr","a6":"normal","a7":"very-long-name-2","a8":"testKeys","a9":"1","aa":"2","ab":"3","ac":"4","ad":"5","ae":"6","af":"7","ag":"8","ah":"9","ai":"10","aj":"11","ak":"12","al":"13","am":"14","an":"15","ao":"16","ap":"17","aq":"18","ar":"19","as":"20","at":"21","au":"22","av":"23","aw":"24","ax":"25","ay":"26","az":"a","b0":"b","b1":"c","b2":"d","b3":"e","b4":"f","b5":"g","b6":"h","b7":"i","b8":"j","b9":"k","ba":"l","bb":"m","bc":"n","bd":"o","be":"p","bf":"q","bg":"r","bh":"s","bi":"t","bj":"u","bk":"v","bl":"w","bm":"x","bn":"y","bo":"z"}}'
     );
   });
