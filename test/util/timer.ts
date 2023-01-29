f0f89decd5fc4aae64df6d2d6fadadaf08a7ef7a
Kit-p <pangkit888@gmail.com>
2023-01-29 08:22:25 +0000

    feat: stringify (compress)

---
+++ test/util/timer.ts
@@ -0,0 +1,27 @@
+/**
+ * @param target target function to measure
+ * @returns Array of [result, durationInMs]
+ */
+export function timer<T>(target: () => T): [T, number];
+export async function timer<T extends Promise<any>>(
+  target: () => T
+): Promise<[Awaited<T>, number]>;
+export function timer(
+  target: () => any
+): [any, number] | Promise<[any, number]> {
+  const start = performance.now();
+  const result = target();
+  if (typeof result?.then === 'function') {
+    return new Promise((resolve, reject) => {
+      result
+        .then((awaited: any) => {
+          const durationInMs = performance.now() - start;
+          return resolve([awaited, durationInMs]);
+        })
+        .catch((error: any) => reject(error));
+    });
+  } else {
+    const durationInMs = performance.now() - start;
+    return [result, durationInMs];
+  }
+}
