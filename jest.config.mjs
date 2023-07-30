109caecd12209ef114b216fc96338bf63ec955f2
Kit-p <pangkit888@gmail.com>
2023-07-30 09:31:18 +0000

    chore(test): configure jest threshold

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,6 +8,14 @@ const config = {
   transform: {
     "^.+\\.(c|m)?(t|j)sx?$": ["@swc/jest"],
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
 
 export default config;
